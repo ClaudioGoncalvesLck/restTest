8a5bb46883de2abf310e2eabff807aa7f7f81b81
claudiogoncalveslck <claudiogoncalveslck@gmail.com>
2022-05-06 12:36:39 +0100

    added seeders and faker.js

---
+++ db/seeds/products_seed.js
@@ -0,0 +1,19 @@
+const faker = require("faker");
+const createFakeProduct = () => ({
+  name: faker.commerce.productName(),
+  price: faker.commerce.price(1, 500, 2),
+});
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.seed = async function (knex) {
+  await knex("products").del();
+  const fakeProducts = [];
+  const fakeProductsAmount = 10;
+  for (let i = 0; i < fakeProductsAmount; i++) {
+    fakeProducts.push(createFakeProduct());
+  }
+  await knex("products").insert(fakeProducts);
+};
