4f182ccc49c97c5c15ecb6788bd1ca0ce73efd59
claudiogoncalveslck <claudiogoncalveslck@gmail.com>
2022-05-11 12:01:57 +0100

    JWT and dependencies

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
+require("dotenv").config();
 //ROUTES
 const user = require("./routes/user.js");
 const product = require("./routes/product.js");
+const auth = require("./routes/auth.js");
+
+app.use(express.json());
 app.use("/user", user);
 app.use("/product", product);
+app.use("/auth", auth);
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
